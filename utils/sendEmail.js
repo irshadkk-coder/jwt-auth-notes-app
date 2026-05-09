c6bbb919b7739fb220abe97ca9c6f62d3b4a4d34
irshad <irshadkk519082@gmail.com>
2026-05-09 18:41:12 +0530

    debug email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,26 +2,36 @@ import nodemailer from 'nodemailer'
 
 
 
-const sendEmail=async(options)=>{
-      console.log("📧 Sending email to:", options.to); 
-   const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  family: 4,        // ✅ only this needs to be added
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const sendEmail = async (options) => {
+  try {
 
+    console.log("📧 Sending email to:", options.to);
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
     await transporter.sendMail({
-        from:process.env.EMAIL_USER,
-        to:options.to,
-        subject:options.subject,
-        text:options.text,
-    })
-     
-  console.log("✅ Email sent successfully");
-}
+      from: process.env.EMAIL_USER,
+      to: options.to,
+      subject: options.subject,
+      text: options.text,
+    });
+
+    console.log("✅ Email sent successfully");
+
+  } catch (error) {
+
+    console.log("❌ EMAIL ERROR:", error);
+
+  }
+};
 
 export default sendEmail;
