63c606093629b6e0447334dd615636276a989c08
irshad <irshadkk519082@gmail.com>
2026-05-10 09:56:12 +0530

    using resend instead of nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,36 +1,26 @@
-import nodemailer from 'nodemailer'
+import { Resend } from "resend";
 
-
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
   try {
 
     console.log("📧 Sending email to:", options.to);
 
-  const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
-  requireTLS: true,  // ✅ add this
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
       to: options.to,
       subject: options.subject,
       text: options.text,
     });
 
-    console.log("✅ Email sent successfully");
+    console.log("✅ Email sent:", data);
 
   } catch (error) {
 
     console.log("❌ EMAIL ERROR:", error);
 
+    throw error;
   }
 };
 
